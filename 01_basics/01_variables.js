96b4e0355d8d72c85f43e4ff5ff824475b7f1d08
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-09 10:51:54 +0000

    configuration done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,10 @@ let accountEmail = "biswaranjandas@vjd.com"
 var accountPassword = "12345"
 accountCity = "jaypur "
 
+<<<<<<< HEAD
+console.log([ accountId, accountEmail, accountPassword, accountCity]);
+
+=======
  
 
 accountEmail ="bdjjdihndidlakcj"
@@ -10,3 +14,4 @@ accountPassword = "5444475"
 accountCity = "Bangaluru"
 
 console.table([ accountId, accountEmail, accountPassword, accountCity]);
+>>>>>>> 75365bc8ede1df61112154e03185aac4bba1f23c
