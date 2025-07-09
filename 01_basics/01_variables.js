75365bc8ede1df61112154e03185aac4bba1f23c
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-09 10:44:26 +0000

    container configuration done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "biswaranjandas@vjd.com"
+var accountPassword = "12345"
+accountCity = "jaypur "
+
+ 
+
+accountEmail ="bdjjdihndidlakcj"
+accountPassword = "5444475"
+accountCity = "Bangaluru"
+
+console.table([ accountId, accountEmail, accountPassword, accountCity]);
