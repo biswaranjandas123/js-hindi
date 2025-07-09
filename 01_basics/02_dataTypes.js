e359de7b4ce3ceeaf1ae7ef0bd1d195d6efa35cf
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-09 13:16:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all js code as newer version
+
+//alet (3+3)//we are using nodejs, not browser
+
+console.log (3
+    +3)     //codes are right but not for good redable so don't do that//
+
+console.log ("Biswa")
+
+let name = "Biswa"     //string data type 
+let age = 21            //number
+let isLoggedIn = false   // boolean type data type
+
+
+// below describe abot data type
+
+// number => 2 to power 53
+//bigint (if the number are very big this bigint use)
+//string=> " "  (use this single coart or dobule coart  -- best is dobule coart)
+//boolean => true/false  ( always give the answer by the true or false)
+//null =>standalone value (famous and imp) (mean empty)
+//undefine =>  (mean declair variable not valu) example= let state;  her value state name which cannt declaired
+//symbol=> (use for unique ness)
+
+
+//object
+
+console.log(typeof "age");   //out put number
+console.log(typeof null)
