22d1458be1a4473ba0f5a9074791a45ebe863c5e
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-09 10:58:14 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,7 @@
 const accountId = 144553
 let accountEmail = "biswaranjandas@vjd.com"
 var accountPassword = "12345"
-accountCity = "jaypur "
 
-<<<<<<< HEAD
-console.log([ accountId, accountEmail, accountPassword, accountCity]);
-
-=======
  
 
 accountEmail ="bdjjdihndidlakcj"
@@ -14,4 +9,3 @@ accountPassword = "5444475"
 accountCity = "Bangaluru"
 
 console.table([ accountId, accountEmail, accountPassword, accountCity]);
->>>>>>> 75365bc8ede1df61112154e03185aac4bba1f23c
