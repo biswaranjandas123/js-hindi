124af314166c510fe512a00d25c8619e31249322
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-10 15:05:13 +0000

    ConversionDatatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,39 +1,71 @@
-let score = 33          //test and put the value 33,null,undifiend,true and false  what the terminal found show it//
+// let score = 33          //test and put the value 33,null,undifiend,true and false  what the terminal found show it//
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
-
-//note:-
-
-//"33"=> terminal output 33
-//null=> 0
-//true=> 1
-//false=> 0
-//undefined=> NaN (Not a Number)
-//"334aajsb"=> NaN (Not a Number)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
-let isLoggedIn = 0
+// //note:-
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// //"33"=> terminal output 33
+// //null=> 0
+// //true=> 1
+// //false=> 0
+// //undefined=> NaN (Not a Number)
+// //"334aajsb"=> NaN (Not a Number)
 
-// if the value put in isloggedin =0,1,true,false,"","Biswa"
-//Answer will be in terminal = false,true,true,false,false,true respectively..............
 
 
-let haveNumber = "33"
+// let isLoggedIn = 0
 
-let stringHaveNumber = String(haveNumber);
-console.log(stringHaveNumber);
-console.log(typeof(stringHaveNumber))
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// // if the value put in isloggedin =0,1,true,false,"","Biswa"
+// //Answer will be in terminal = false,true,true,false,false,true respectively..............
+
+
+// let haveNumber = "33"
+
+// let stringHaveNumber = String(haveNumber);
+// console.log(stringHaveNumber);
+// console.log(typeof(stringHaveNumber))
 
 // Her we found that 33 is a string ...terminal output=33 .
 // ...but when we code console.log(typeof(stringhavenumber)) out put will be string not a number...
+
+// *************************************** Operation **********************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//string addition 
+let str1 = "hello"
+let str2 = " Biswa"
+let str3 = str1 + str2
+console.log(str3);        //in the case output = hello Biswa
+
+
+console.log(1+"2");       //output12
+console.log("1"+2);       //12
+console.log("1"+2+2);     //122
+console.log(1+2+"2");     //32     her the string only 2 1+2=3...so the number is 32
+console.log(1+"2"+2);     //122
+
+
+console.log(+true);   //output = 1
+console.log(+"");     //output = 0
+
+
+let gameCounter = 100     // gamecounter value always increasses
+gameCounter++;  // her  we called prefix and postfix ...please read in.." js mdn".. searching//out put=101 increase
+console.log(gameCounter);
+  
+
+
+//****************if u want to read this topic, then go to "ECMAScript 2024" => then go left 7option "ABSTRACT OPERATION"****/
