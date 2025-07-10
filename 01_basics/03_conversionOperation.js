e0369cda3b2437c659139753afda5a5e06e1020f
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-10 15:10:31 +0000

    ConversionDatatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,39 +1,39 @@
-// let score = 33          //test and put the value 33,null,undifiend,true and false  what the terminal found show it//
+let score = 33          //test and put the value 33,null,undifiend,true and false  what the terminal found show it//
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
-
-
-
-// //note:-
-
-// //"33"=> terminal output 33
-// //null=> 0
-// //true=> 1
-// //false=> 0
-// //undefined=> NaN (Not a Number)
-// //"334aajsb"=> NaN (Not a Number)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 
 
-// let isLoggedIn = 0
+//note:-
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn);
+//"33"=> terminal output 33
+//null=> 0
+//true=> 1
+//false=> 0
+//undefined=> NaN (Not a Number)
+//"334aajsb"=> NaN (Not a Number)
 
-// // if the value put in isloggedin =0,1,true,false,"","Biswa"
-// //Answer will be in terminal = false,true,true,false,false,true respectively..............
 
 
-// let haveNumber = "33"
+let isLoggedIn = 0
 
-// let stringHaveNumber = String(haveNumber);
-// console.log(stringHaveNumber);
-// console.log(typeof(stringHaveNumber))
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// if the value put in isloggedin =0,1,true,false,"","Biswa"
+//Answer will be in terminal = false,true,true,false,false,true respectively..............
+
+
+let haveNumber = "33"
+
+let stringHaveNumber = String(haveNumber);
+console.log(stringHaveNumber);
+console.log(typeof(stringHaveNumber))
 
 // Her we found that 33 is a string ...terminal output=33 .
 // ...but when we code console.log(typeof(stringhavenumber)) out put will be string not a number...
