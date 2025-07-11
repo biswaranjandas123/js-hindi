53122dd0cfa2a6d06ce10ef47fead72624265efb
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-07-11 11:23:36 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+console.log(2>1);    //output=true
+console.log(2>=1);   //true
+console.log(2==1);   //false
+console.log(2!=1);   //true
+
+
+console.log("2">1);   //true
+
+
+// console.log(null>0);     //false
+// console.log(null==0);    //false   // Always avoid this type of comparision like null,undifined//
+// console.log(null>=0);    //true
+
+
+//imp describe at below
+
+//"===" strick check...not only check the value but also stricly check data type example below
+console.log("2"===2);   //her data type different "2" is string and 2 only number //her output=false
+
+
+
+
+
+
