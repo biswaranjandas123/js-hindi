eacf68b39d99a271e6fc369285d9a91175122d3f
biswaranjandas123 <biswaranjandas453@gmail.com>
2025-08-02 09:32:49 +0530

    variable change

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,16 @@
-const accountId = 144553
+const accountId = 14453                        //const meaning once value add hele change hbni(value lock hoijib) //
 let accountEmail = "biswaranjandas@vjd.com"
-var accountPassword = "12345"
+var accountPassword = "12345"                   //her the email and passowrd written in "".....//
+/* var cannot use mostly they are creat many problem only use the "let" and "const"*/
+let accountState;   //the output is undefined//
 
- 
+
 
 accountEmail ="bdjjdihndidlakcj"
 accountPassword = "5444475"
 accountCity = "Bangaluru"
 
-console.table([ accountId, accountEmail, accountPassword, accountCity]);
+console.log(accountEmail);          //output bdjjdi
+
+console.table([ accountId, accountEmail, accountPassword, accountCity,accountState]);
+
